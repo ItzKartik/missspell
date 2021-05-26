4a92f672a76313122ad787e2e899b6ec5f038426
Itzkartik <smartzboi@gmail.com>
2021-05-26 09:31:31 +0530

    work

--- checker/settings.py
+++ checker/settings.py
@@ -117,7 +117,8 @@ USE_L10N = True
 USE_TZ = True
 
 X_FRAME_OPTIONS = 'ALLOW-FROM https://missspell.com/'
-
+CSRF_TRUSTED_ORIGINS = ['missspell.com']
+CSRF_COOKIE_SAMESITE = None
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
